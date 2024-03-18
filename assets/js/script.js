e93bed093d5eeba18112a7a0f80f5d9a7a3b281a
Patteerrik . <patrik.akerbergz@gmail.com>
2024-03-18 12:10:06 +0000

    add questions to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+const myQuestions = [
+    {
+    question:("Who holds the world record in 100 meter sprint?"),
+    answer: {
+        a: "Tyson Gay",
+        b: "Asafa Powell",
+        c: "Maurice Green",
+        d: "Usain Bolt"
+    },
+    correctAnswer: "d"
+},
+
+   {
+    question:("Which country has won the most Worlds cups in football?")
+    answer: {
+        a: "Netherlands",
+        b: "Germany",
+        c: "Brazil"
+        d: "Argentina";
+    },
+    correctAnswer: "c"
+},
+   
+{
+    question:(""),
+    answer: {
+        a: "Tyson Gay",
+        b: "Asafa Powell",
+        c: "Maurice Green",
+        d: "Usain Bolt"
+    },
+    correctAnswer: ""
+},
+
+{
+    question:(""),
+    answer: {
+        a: "",
+        b: "",
+        c: "",
+        d: ""
+    },
+    correctAnswer: ""
+},
+
+{
+    question:(""),
+    answer: {
+        a: "",
+        b: "",
+        c: "",
+        d: ""
+    },
+    correctAnswer: ""
+},
+]
