4b1ea9e7c998986fcb0466855e5ac1aa92bdd7e8
Patteerrik . <patrik.akerbergz@gmail.com>
2024-03-19 14:12:11 +0000

    add text and brackets from question in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,57 +3,57 @@
 */
 var myQuestions = [
     {
-    question:("Who holds the world record in 100 meter sprint?"),
-    answer: {
+    question:"Who holds the world record in 100 meter sprint?",
+    
         a: "Tyson Gay",
         b: "Asafa Powell",
         c: "Maurice Green",
         d: "Usain Bolt",
-    },
+    
     correctAnswer: "d"
 },
 
     {
-    question:("Which country has won the most World cups in football?"),
-    answer: {
+    question:"Which country has won the most World cups in football?",
+    
         a: "Netherlands",
         b: "Germany",
         c: "Brazil",
         d: "Argentina",
-    },
+    
     correctAnswer: "c"
 },
    
     {
-    question:("Who was the premier league top scorer 22/23?"),
-    answer: {
+    question:"Who was the premier league top scorer 22/23?",
+    
         a: "Marcus Rashford",
         b: "Mohamed Salah",
         c: "Harry Kane",
         d: "Erling Haaland",
-    },
+    
     correctAnswer: "d"
 },
 
     {
-    question:("In which sport do teams compete to win the Stanley Cup?"),
-    answer: {
+    question:"In which sport do teams compete to win the Stanley Cup?",
+    
         a: "Tennis",
         b: "Hockey",
         c: "Rugby",
         d: "Basketball",
-    },
+    
     correctAnswer: "b"
 },
 
     {
-    question:("How many career goals has Lionel Messi scored?"),
-    answer: {
+    question:"How many career goals has Lionel Messi scored?",
+    
         a: "826",
         b: "629",
         c: "971",
         d: "582",
-    },
+    
     correctAnswer: "a"
 },
 ];
