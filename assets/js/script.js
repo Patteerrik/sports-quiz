d597a4df9e1c39bccc3226f3be1ac6887d8f9522
Patteerrik . <patrik.akerbergz@gmail.com>
2024-03-20 14:23:06 +0000

    add changed typo

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,31 +13,31 @@ let submitButton = document.getElementById("submit");
 var myQuestions = [
     {
     question:"Who holds the world record in 100 meter sprint?",
-    options: ["Tyson Gay", "Asafa Powell", "Maurice Green", "Usain Bolt"];
+    options: ["Tyson Gay", "Asafa Powell", "Maurice Green", "Usain Bolt"],
     answer: "Usain Bolt"
 },
 
     {
     question:"Which country has won the most World cups in football?",
-    options: ["Netherlands", "Germany", "Brazil", "Argentina"];
+    options: ["Netherlands", "Germany", "Brazil", "Argentina"],
     answer: "Brazil"
 },
    
     {
     question:"Who was the premier league top scorer 22/23?",
-    options: ["Harry Kane", "Erling Haaland", "Mohammed Salah", "Marcus Rashford"];
+    options: ["Harry Kane", "Erling Haaland", "Mohammed Salah", "Marcus Rashford"],
     answer: "Erling Haaland"
 },
 
     {
     question:"In which sport do teams compete to win the Stanley Cup?",
-    options: ["Tennis", "Hockey", "Rugby", "Basketball",];
+    options: ["Tennis", "Hockey", "Rugby", "Basketball",],
     answer: "Hockey"
 },
 
     {
     question:"How many career goals has Lionel Messi scored?",
-    options: ["More than 800", "more than 600", "More than 1000", "More than 900"];
+    options: ["More than 800", "more than 600", "More than 1000", "More than 900"],
     answer: "More than 800"
 },
 ];
