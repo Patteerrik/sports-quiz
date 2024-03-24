dc8812f8f986ebd496e7a5c525f245f27c063c12
Patteerrik . <patrik.akerbergz@gmail.com>
2024-03-24 10:42:40 +0000

    add removed unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,9 +41,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let currentQuestion = 0;
 
-    function startQuiz () {
-         
-        showQuestion();
+    function startQuiz () {}
+
+    function showAnswer (currentQuestion) {
+        if (currentQuestion < myQuestions.length && currentQuestion >= 0) {
+            document.getElementById("question")[0].innerHTML = myQuestions[myQuestions].question;
+        }
     }
 
     // vaiables
@@ -57,39 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     submitButton.addEventListener("click", result);
 
-    function answer () {
-        var userAnswer;
-
-        if (userAnswer === myQuestions.answer) {
-            alert("That is correct!");
-        } 
-
-        currentQuestion++;
-
-        if (userAnswer === myQuestions.length) {
-            alert("Contratulations, you´ve finished")
-        } else {
-            loadQuestion(currentQuestion);
-        }
-    }
-
-    function options () {
-        
-        var Answer1 = document.getElementById(a);
-        var Answer2 = document.getElementById(b);
-        var Answer3 = document.getElementById(c);
-        var Answer4 = document.getElementById(d);
-
-        Answer1.innerHTML = question[index].options[0];
-        Answer2.innerHTML = question[index].options[1]
-        Answer3.innerHTML = question[index].options[2]
-        Answer4.innerHTML = question[index].options[3]
-
-
-        
-    }
-
-    loadQuestion(currentQuestion);
+    
 
 
 
