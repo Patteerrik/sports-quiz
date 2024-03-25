a32c7a192908df3a72e900ef33f987112c214970
Patteerrik . <patrik.akerbergz@gmail.com>
2024-03-25 08:48:56 +0000

    add change text on myQuestions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,31 +11,31 @@ document.addEventListener("DOMContentLoaded", function () {
         {
             question: "Who holds the world record in 100 meter sprint?",
             options: ["Tyson Gay", "Asafa Powell", "Maurice Green", "Usain Bolt"],
-            answer: "Usain Bolt",
+            rightAnswer: "Usain Bolt",
         },
 
         {
             question: "Which country has won the most World cups in football?",
             options: ["Netherlands", "Germany", "Brazil", "Argentina"],
-            answer: "Brazil",
+            rightAnswer: "Brazil",
         },
 
         {
             question: "Who was the premier league top scorer 22/23?",
             options: ["Harry Kane", "Erling Haaland", "Mohammed Salah", "Marcus Rashford"],
-            answer: "Erling Haaland",
+            rightAnswer: "Erling Haaland",
         },
 
         {
             question: "In which sport do teams compete to win the Stanley Cup?",
             options: ["Tennis", "Hockey", "Rugby", "Basketball",],
-            answer: "Hockey",
+            rightAnswer: "Hockey",
         },
 
         {
             question: "How many career goals has Lionel Messi scored?",
             options: ["More than 800", "less than 700", "More than 1000", "More than 900"],
-            answer: "More than 800",
+            rightAnswer: "More than 800",
         },
     ];
 
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
 
         }
-        
+
     function showAnswer() {
         var currentAnswer = myQuestions[questionIndex].answer;
         if (selectedAnswer === rightAnswer){
