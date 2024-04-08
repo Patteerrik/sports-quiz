b92d16ca0e9673b3e19b69d78ecb137a61a1771f
Patteerrik . <patrik.akerbergz@gmail.com>
2024-04-08 09:44:11 +0000

    add change in eventlistener to make rulesbutton appear after one click

--- assets/js/rules.js
+++ assets/js/rules.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (rulesWindow.style.display === "none") {
     rulesWindow.style.display = "block";
     } else {
-    rulesWindow.style.display = "none";
+    rulesWindow.style.display = "block";
     } 
     });
     closeRulesButton.addEventListener("click", function() {
